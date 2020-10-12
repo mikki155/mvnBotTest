df828a98022c141a359aac4693ce6a4c25f40607
Mikael Haug <mikael.haug@confirmit.com>
2020-10-12 11:46:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a repo for testing automated updates for Maven dependencies
---
+++ src/main/java/com/mikki155/github/App.java
@@ -0,0 +1,9 @@
+package com.mikki155.github;
+
+import org.apache.commons.lang3.math.Fraction;
+
+public class App {
+  public static void main(String [] args) {
+    System.out.println(Fraction.getFraction(3, 4));
+  }
+}
